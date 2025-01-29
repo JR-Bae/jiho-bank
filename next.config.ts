2509f04baaf19c89b88be5a14f3676a7d48e7664
bigb <bigb.deb.system@gmail.com>
2025-01-30 02:04:16 +0900

    blob 주소 추가

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone', // Cloudflare Pages 호환 모드
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*.public.blob.vercel-storage.com',
+                port: '',
+                pathname: '/uploads/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
